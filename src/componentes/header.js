13a9eb0f604d2d0ba6e74dce745143bc657f9a6a
caioCodigo <caio_pfl@hotmail.com>
2018-03-24 03:04:02 -0300

    cartoes emcaminhados

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -7,10 +7,28 @@ class Header extends Component {
                 <div className="row linha-card">
 
                     <div className="col-2  bloco-atras1" >
+                    
                     </div>
+
                         <div className="col-4 bloco">
-                        
+
+                            <div className="titulo-card" >
+                                <p>Estude oque você gosta</p>
+                            </div>
+
+                            <div className="conteudo-imagem" >
+                                <img className="imagem-card"  src="card1.jpg" />
+                            </div>
+
+                            <div className="botao-card">
+                               <div className="testao"><span class="seta-direita"></span>
+                               <button className="btn-block botao" >explorar</button>   </div>          
+                              
+                               
+                            </div>
+
                         </div>
+
                     <div className="col-2 bloco-atras2" >
                     </div>   
                 </div>
